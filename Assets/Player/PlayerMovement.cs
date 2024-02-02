ce9ef23010b2598a2680725dcf2d37047b65d835
Gryphon2048 <21gwh4@queensu.ca>
2024-02-02 14:23:36 -0500

    Merged Jack's Input with Gwen's Movement
    This is also when I was having problems with my laptop. If there is an issue it may be here.

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed = 12.5f;
 
     public Rigidbody2D rb;
     public Animator animator;
+    public InputController playerActions;
 
-    Vector2 movement;
+    Vector2 movementRaw;
+    Vector3 movement = new Vector3(0,0,0);
 
     // Update is called once per frame
     void Update()
     {
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
@@ -26,7 +28,24 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        movementRaw = (Vector2) playerActions.Player.Move.ReadValue<Vector2>();
+        movement = new Vector3(movementRaw.x, movementRaw.y, 0);
         // Movement
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        transform.position = transform.position + movement * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    void Awake()
+    {
+        playerActions = new InputController();
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerActions.Player.Disable();
     }
 }
