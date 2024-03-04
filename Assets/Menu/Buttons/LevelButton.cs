88814a2018cb73237893c1b6c08363f6face5dcd
Jack Hickey <jack.hickey@rogers.com>
2024-03-03 21:26:48 -0500

    Started Level Menu
    the level buttons generate programatically. Working on inputs, hooking them up to levels, etc.

---
+++ Assets/Menu/Buttons/LevelButton.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour
+{
+    public string loadScene;
+    [Range(0, 99)]
+    public int LevelNumber;
+
+    private Sprite[] numberSprites;
+    private SpriteRenderer spriteRenderer;
+    private float spriteHeight;
+    private Vector3 highlightVec;
+    private PolygonCollider2D thisCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sprite[] numberSprites = Resources.LoadAll<Sprite>("LevelButtons1");
+
+        // Determines the left and right sides of the level button by the number assigned to the button
+        char[] nameChars = levelName(LevelNumber);
+        string left = "l" + int.Parse(nameChars[0].ToString());
+        string right = "r" + int.Parse(nameChars[1].ToString());
+        int breakLoop = 0;
+
+        // Assigns the proper sprites to the left and rights sides of the button.
+        foreach (Sprite number in numberSprites) {
+            if (number.name == left) {
+                transform.Find("left").GetComponent<SpriteRenderer>().sprite = number;
+                breakLoop++;
+            } else if (number.name == right) {
+                transform.Find("right").GetComponent<SpriteRenderer>().sprite = number;
+                breakLoop++;
+            }
+
+            if (breakLoop == 2) break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    char[] levelName(int val) {
+        string name = val.ToString("00");
+        return name.ToCharArray();
+    }
+}
