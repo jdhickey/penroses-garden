a8b503ad018b098bee90870ab3e71696837a2b22
Scholtzz <hagan.gwendolyn@gmail.com>
2024-03-16 18:05:17 -0400

    Quit Button

--- Assets/Inventory and UI/RuntimeInventoryUI.cs
+++ Assets/Inventory and UI/RuntimeInventoryUI.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEditor.UIElements;
 
 public class RuntimeInventoryUI : MonoBehaviour
 {
