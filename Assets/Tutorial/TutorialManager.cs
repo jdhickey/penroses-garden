bdde5b0f21668ba0afc82591be9044f19a2310f6
Jack Hickey <jack.hickey@rogers.com>
2024-03-23 15:44:53 -0400

    Further tutorial work
    I've added the custom font and art into the game, and built logic which determines strings to display for each tutorial stage.
    
    I am close to implementing the checks for each tutorial stage, however I am struggling with it as there is not scoring system as of yet. Method TBD.

--- Assets/Tutorial/TutorialManager.cs
+++ Assets/Tutorial/TutorialManager.cs
@@ -1,54 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
 public class TutorialManager : MonoBehaviour
 {
+    public GameObject tutorialBox;
+
     private ReadOnlyArray<InputBinding> bindings;
-    private List<string> move;
-    private List<string> place;
-    private List<string> select;
-    private List<string> scroll;
-    private List<string> rotate;
-    private List<string> shuffle;
+    private string move;
+    private string place;
+    private string select;
+    private string scroll;
+    private string rotate;
+    private string shuffle;
 
     // Start is called before the first frame update
     void Start()
     {
+        // The below code iterates through all of the different key bindings, assigning them to the "action"
+        // that the player takes using that binding.
+
+        // It then formats the bindings into a string which can be used in the tutorial to explain how to take an action.
+
         bindings = GameObject.FindObjectOfType<PlayerInput>().actions.FindActionMap("Player").bindings;
-        move = new List<string>();
-        place = new List<string>();
-        select = new List<string>();
-        scroll = new List<string>();
-        rotate = new List<string>();
-        shuffle = new List<string>();
+        List<string> moveList = new List<string>();
+        List<string> placeList = new List<string>();
+        List<string> selectList = new List<string>();
+        List<string> scrollList = new List<string>();
+        List<string> rotateList = new List<string>();
+        List<string> shuffleList = new List<string>();
 
         for (int i = 0; i < bindings.Count; i++) {
             string displayString = bindings[i].ToDisplayString();
-
-            if (string.IsNullOrEmpty(displayString)) continue;
+            displayString.Replace("/", " ");
+            if (string.IsNullOrEmpty(displayString)) {
+                continue;
+            }
 
             if (bindings[i].action == "Move") {
-                move.Add(displayString);
+                moveList.Add(displayString);
             } else if (bindings[i].action == "Place") {
-                place.Add(displayString);
+                placeList.Add(displayString);
             } else if (bindings[i].action == "Inventory Select") {
-                select.Add(displayString);
+                selectList.Add(displayString);
             } else if (bindings[i].action == "Inventory Scroll") {
-                scroll.Add(displayString);
+                scrollList.Add(displayString);
             } else if (bindings[i].action == "Inventory Rotate") {
-                rotate.Add(displayString);
+                rotateList.Add(displayString);
             } else if (bindings[i].action == "Shuffle") {
-                shuffle.Add(displayString);
+                shuffleList.Add(displayString);
             } 
         }
+
+        // This sets the display strings for each action.
+        move = BindingsToString(moveList);
+        place = BindingsToString(placeList);
+        select = BindingsToString(selectList);
+        scroll = BindingsToString(scrollList);
+        rotate = BindingsToString(rotateList);
+        shuffle = BindingsToString(shuffleList);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private static string BindingsToString(List<string> arr) {
+        // This joins together the multiple strings representing the control bindings
+        // into one string which is suitable for display in the tutorial.
+
+        // It places single characters on the same line, but multichar bindings on individual lines.
+        // It replaces control path '/' with a space, assuming that will be human readable
+        // It converts the final string to lowercase and removes trailing whitespace.
+        string display = arr[0];
+
+        for (int i = 1; i < arr.Count; i++) {
+            string sep;
+
+            if (arr[i].Length > 1) {
+                sep = "\n";
+            } else {
+                sep = ", ";
+            }
+
+            display = display + sep + arr[i];
+        }
+
+        return display.ToLower().Trim().Replace("/", " ");
     }
 }
