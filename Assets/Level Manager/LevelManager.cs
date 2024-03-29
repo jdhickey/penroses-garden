149fcd20a03bd0bdadf81128a05a07d273d60b37
Scholtzz <hagan.gwendolyn@gmail.com>
2024-03-28 23:23:43 -0400

    Minor Tweaks
    The build was working! Yay!

--- Assets/Level Manager/LevelManager.cs
+++ Assets/Level Manager/LevelManager.cs
@@ -9,8 +9,8 @@ public static class LevelManager
     public static bool pointPerTile = false; // Should the player earn 1 point per tile?
     public static int randomPreTile = 0; // How many pre-placed tile should there be?
     public static int winThreshold = 1000;
-    public static bool pointPerConnection = true;
+    public static bool pointPerConnection = false;
     public static bool won = false;
     public static bool lost = false;
-    public static float timerVal = 600.0f; // Time in minutes that the clock should be set to.
+    public static float timerVal = 0.0f; // Time in minutes that the clock should be set to.
 }
