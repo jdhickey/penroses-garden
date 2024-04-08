b3f245b41c4bc4b588858d7fa390432bcf8122db
Scholtzz <hagan.gwendolyn@gmail.com>
2024-04-07 20:52:04 -0400

    Made a Level Info Box in the Pause Menu
    I made a little box that displays level information in the pause menu, stuff like how you're getting points, win conditions, etc.

---
+++ Assets/PauseMenuInfo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PauseMenuInfo : MonoBehaviour
+{
+    public TextMeshProUGUI levelInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelInfo = GetComponent<TextMeshProUGUI>();
+        string levelInformation = "";
+        if (LevelManager.currLevel == -1){
+            levelInformation += "Endless\n";
+        }
+        else{
+            levelInformation += "Level " + LevelManager.currLevel.ToString() + "\n";
+        }
+        if (LevelManager.timerVal > 0){
+            levelInformation += "- " + (((int) LevelManager.timerVal)/60+1).ToString() + " mins. on the timer\n";
+        }
+        if (LevelManager.pointPerTile){
+            levelInformation += "- point per tile\n";
+        }
+        if (LevelManager.pointPerConnection){
+            levelInformation += "- point per connection\n";
+        }
+        if (LevelManager.winThreshold > 0){
+            levelInformation += "- " + LevelManager.winThreshold.ToString() + " points to win\n";
+        }
+        if (LevelManager.initialTile){
+            levelInformation += "- initial tile\n";
+        }
+        if (LevelManager.randomPreTile > 0){
+            levelInformation += "- " + LevelManager.randomPreTile + " pre-placed tiles\n";
+        }
+        if (LevelManager.winBySurround > 0){
+            levelInformation += "- " + LevelManager.winBySurround + " tiles to surround\n";
+        }
+        levelInformation = levelInformation.Substring(0, levelInformation.Length-1);
+        levelInfo.SetText(levelInformation);
+    }
+}
